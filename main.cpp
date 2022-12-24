2be22d620e8fd27276bd7b33f471c8b7cfab872a
jasonpogi <51401355+mgalang229@users.noreply.github.com>
2022-12-24 10:24:30 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,9 +2,9 @@ class Solution {
 public:
     uint32_t reverseBits(uint32_t n) {
         uint32_t ans = 0;
-        for (int bit = 31, new_bit = 0; bit >= 0; bit--, new_bit++)
+        for (int bit = 31; bit >= 0; bit--)
             if ((1 << bit) & n)
-                ans += (1 << new_bit);
+                ans += 1 << (31 - bit);
         return ans;
     }
 };
